46f5bedadb52c6341e152c003bea48a671b75d2e
gregshevchenko3 <gregshevchenko3@gmail.com>
2021-06-04 16:00:11 +0000

    npmrc added

--- src/server.js
+++ src/server.js
@@ -1,32 +1,38 @@
 const express = require("express");
 const vueServerRenderer = require('vue-server-renderer');
 const path = require('path');
+const {promisify, types} = require('util');
 const fs = require('fs');
 const setupDevServer = require('../configs/setup-dev-server');
+
+const readFile = promisify(fs.readFile);
+
 const app = express();
 
-const createRenderer = bundle => vueServerRenderer.createBundleRenderer(bundle, {
-    runInNewContext: false,
-    template: fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8')
-});
+
+const createRenderer = async bundle =>{
+    return vueServerRenderer.createBundleRenderer(bundle, {
+        runInNewContext: false, 
+        template: await readFile(path.resolve(__dirname, 'index.html'), 'utf-8')
+    });
+};
 
 app.use('/public', express.static(path.resolve(__dirname, './dist')));
 
 let renderer;
 
+console.log('create renderer call');
 if(process.env.NODE_ENV === 'development')
-    setupDevServer(app, serverBundle => {
-        renderer = createRenderer(serverBundle);
-    });
+    setupDevServer(app, serverBundle => renderer = createRenderer(serverBundle));
 else 
     renderer = createRenderer(require('./dist/vue-ssr-server-bundle.json'));
 
 
 app.get('*', async (req, res)=> {
     const context = {url: req.url};
-    let html;
     try {
-        html = await renderer.renderToString(context);
+        renderer = types.isPromise(renderer)?await renderer: renderer;
+        let html = await renderer.renderToString(context);
         res.end(html);
     } 
     catch(error){
